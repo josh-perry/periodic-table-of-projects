e2123a1595c25e175073dda349518ade2be626d0
Josh Perry <joshperry@outlook.com>
2021-03-14 22:47:14 +0000

    Making the linter happy

--- pages/index.vue
+++ pages/index.vue
@@ -3,26 +3,12 @@
     <h1>Periodic Table of Projects</h1>
     <PeriodicTable/>
 
-    <ProjectsModal v-modal="modalOpen"/>
+    <ProjectsModal/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-import PeriodicTable from "./components/PeriodicTable.vue"
-import ProjectsModal from "./components/ProjectsModal.vue"
-
-export default {
-  data() {
-    return {
-      modalOpen: false
-    }
-  },
-  methods: {
-    toggleModal() {
-      this.modalOpen = !this.modalOpen;
-    }
-  }
-};
+export default {};
 </script>
