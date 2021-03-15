165b3be30ae7ddb92b563f539c85560d886624a9
Josh Perry <joshperry@outlook.com>
2021-03-15 21:21:51 +0000

    Removed stray console.log

--- store/selectedElement.js
+++ store/selectedElement.js
@@ -5,7 +5,6 @@ export const state = () => ({
 
 export const mutations = {
   async select(state, elementName) {
-    console.log(this.$config);
     state.elementName = elementName;
     state.projects = await this.$http.$get(`${this.$config.baseUrl}/json/${elementName}.json`);
   },
